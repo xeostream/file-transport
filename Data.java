e2dc4f7d26d5976ba6363f773656bc8edbe937d1
xeostream <iostreamin@gmail.com>
2013-09-13 14:47:50 +0800

    package parts of file

--- Data.java
+++ Data.java
@@ -2,11 +2,17 @@ import java.io.Serializable;
 
 public class Data implements Serializable {
 	private final static long serialVersionUID = 1l;
-	private long startPos;
+	private final long startPos;
 	private byte[] buff;
 	
 	public Data(long startPos, byte[] buff) {
 		this.startPos = startPos;
 		this.buff = buff;
+	}
+	public long getStartPos() {
+		return startPos;
+	}
+	public byte[] getBuff() {
+		return buff;
 	}
 }
