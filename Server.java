32112bc210c298e4afaee93357e2f21fff853cac
xeostream <iostreamin@gmail.com>
2013-09-13 14:49:10 +0800

    receiver for multi-thread send a file

--- Server.java
+++ Server.java
@@ -1,57 +1,59 @@
-import java.io.DataInputStream;
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.RandomAccessFile;
 import java.net.ServerSocket;
 import java.net.Socket;
-
 
 public class Server {
 	private final static int PORT = 2013;
 	private Socket client;
 	private ServerSocket server;
-	private DataInputStream dis;
-	private FileOutputStream fos;
-	
+	private ObjectInputStream ois;
+	private RandomAccessFile raf;
+
 	public Server() throws Exception {
 		try {
 			try {
 				server = new ServerSocket(PORT);
 				while (true) {
 					client = server.accept();
-					dis = new DataInputStream(client.getInputStream());
-					String fileName = dis.readUTF();
-					long fileLength = dis.readLong();
-					fos = new FileOutputStream(new File(fileName));
-					byte[] sendBytes = new byte[1024];
+					ois = new ObjectInputStream(client.getInputStream());
+					String fileName = ois.readUTF();
+					long fileLength = ois.readLong();
+					raf = new RandomAccessFile(new File(fileName), "rw");
 					int translen = 0;
-					System.out.println("begin receive file<" + fileName + ">,size<" + fileLength + ">");
+					System.out.println("begin receive file<" + fileName
+							+ ">,size<" + fileLength + ">");
 					while (true) {
 						int read = 0;
-						read = dis.read(sendBytes);
-						if (read == -1)
+						Data data = (Data) ois.readObject();
+						if (data == null)
 							break;
+						read = data.getBuff().length;
 						translen += read;
-						System.out.println("recevie file percent" + 100 * (translen/fileLength));
-						fos.write(sendBytes, 0, read);
-						fos.flush();
+						System.out.println("recevie file percent" + 100
+								* (translen / fileLength));
+						raf.seek(data.getStartPos());
+						raf.write(data.getBuff());
 					}
 					System.out.println("receiver success");
+					raf.close();
 					client.close();
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			} finally {
-				if (dis != null)
-					dis.close();
-				if (fos != null)
-					fos.close();
+				if (ois != null)
+					ois.close();
+				if (raf != null)
+					raf.close();
 				server.close();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		new Server();
 	}
